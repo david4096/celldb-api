d91a0573d8c718152e6acff18f3343d68ecea4c6
David Steinberg <david@resium.com>
2017-07-31 21:07:48 -0700

    Basic web service

---
+++ celldb/api/api.py
@@ -0,0 +1,59 @@
+from klein import run, route
+import numpy as np
+
+from celldb import pd as celldb
+
+import json
+import csv
+import io
+
+URL = "localhost"
+
+@route('/samples')
+def list_samples(request):
+    connection = celldb.connect(URL)
+    return str(list(celldb.list_samples(connection)))
+
+
+@route('/features')
+def list_features(request):
+    connection = celldb.connect(URL)
+    return str(list(celldb.list_features(connection)))
+
+@route('/matrix', methods=['POST'])
+def matrix(request):
+    connection = celldb.connect(URL)
+    request_dict = json.loads(request.content.read())
+    matrix_data = celldb.matrix(
+        connection, request_dict['sample_ids'], request_dict['feature_ids'])
+    ret_dict = {}
+    for row in matrix_data:
+        ret_dict[row[0]] = row[1:]
+    return json.dumps(ret_dict)
+
+@route('/matrix/tsv', methods=['POST'])
+def matrix_tsv(request):
+    connection = celldb.connect(URL)
+    request_dict = json.loads(request.content.read())
+    df = celldb.df(
+        connection, request_dict['sample_ids'], request_dict['feature_ids'])
+    return df.to_csv(sep="\t")
+
+
+@route('/matrix/dataframe', methods=['POST'])
+def matrix_dataframe(request):
+    connection = celldb.connect(URL)
+    request_dict = json.loads(request.content.read())
+    df = celldb.df(connection, request_dict['sample_ids'], request_dict['feature_ids'])
+    return df.to_string()
+
+
+@route('/matrix/html', methods=['POST'])
+def matrix_html(request):
+    connection = celldb.connect(URL)
+    request_dict = json.loads(request.content.read())
+    df = celldb.df(connection, request_dict['sample_ids'], request_dict['feature_ids'])
+    return df.to_html()
+
+def main(args=None):
+    run("localhost", 8080)
